82ad58288a5325dd573803014fac9b8f79e0d6d7
UnDeadlyDev <87789394+UnDeadlyDev@users.noreply.github.com>
2025-08-03 21:40:58 -0400

    remove stay time actionbar

--- src/main/java/com/undeadlydev/UTA/managers/ActionBarManager.java
+++ src/main/java/com/undeadlydev/UTA/managers/ActionBarManager.java
@@ -2,6 +2,7 @@ package com.undeadlydev.UTA.managers;
 
 import com.cryptomorin.xseries.messages.ActionBar;
 import com.undeadlydev.UTA.Main;
+import net.md_5.bungee.api.chat.BaseComponent;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
@@ -60,22 +61,19 @@ public class ActionBarManager {
 
     public void sendAcOnPremium(Player player) {
         String message = plugin.getLang().get(player, "actionbar.autologin");
-        int time = plugin.getConfig().getInt("config.actionbar.autologin.time.stay");
         ActionBar.clearActionBar(player);
-        ActionBar.sendActionBar(plugin , player, message, time);
+        ActionBar.sendActionBar(plugin , player, message);
     }
 
     public void sendAcOnRegister(Player player) {
         String message = plugin.getLang().get(player, "actionbar.register");
-        int time = plugin.getConfig().getInt("config.actionbar.register.time.stay");
         ActionBar.clearActionBar(player);
-        ActionBar.sendActionBar(plugin , player, message, time);
+        ActionBar.sendActionBar(plugin , player, message);
     }
 
     public void sendAcOnLogin(Player player) {
         String message = plugin.getLang().get(player, "actionbar.login");
-        int time = plugin.getConfig().getInt("config.actionbar.login.time.stay");
         ActionBar.clearActionBar(player);
-        ActionBar.sendActionBar(plugin , player, message, time);
+        ActionBar.sendActionBar(plugin , player, message);
     }
 }
